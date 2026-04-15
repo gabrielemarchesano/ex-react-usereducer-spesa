20988bb88e68b598131d2465b8faa82785d0da66
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-15 16:44:48 +0200

    milestone 1: show products' list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 function App() {
 
-  return(
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 }
+  ]
+
+  return (
     <>
-    
+      <h1>Lista dei prodotti</h1>
+      <ul>
+        {
+          products.map((product, index) => (
+            <li key={index}>
+              <h2>Prodotto {index + 1}</h2>
+              <p>Nome: {product.name}</p>
+              <p>Prezzo: {product.price.toFixed(2)} €</p>
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
-
 }
 
 export default App
